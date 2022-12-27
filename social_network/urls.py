0ca8e06efcb248948c387a62b2d5adacb0341d00
Dhananjayan <dhananjayan.online@outlook.com>
2022-12-27 11:52:43 +0530

    static file url mapping added in project settings.py file

--- social_network/urls.py
+++ social_network/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register("users", views.UserView, basename="users")
@@ -23,4 +26,5 @@ router.register("posts", views.PostView, basename="posts")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+router.urls
+    path('web/', include("webapp.urls"))
+]+router.urls+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
