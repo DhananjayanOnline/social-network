c089aa7af640d42245283769ca842b80cb8e2bb6
Dhananjayan <dhananjayan.online@outlook.com>
2022-12-27 12:37:01 +0530

    UserRegistrationForm Created

---
+++ webapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from api.models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password1',
+            'password2',
+            'first_name',
+            'last_name',
+            'email'
+        ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from api.models import *
+from .forms import *
 
 # Create your views here.
+
